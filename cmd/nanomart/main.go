8c4cd64026bcef435331f6dae38f71111d93b9f5
Anton Kilchik <anton.kilchik@gett.com>
2023-01-06 14:37:04 +0200

    add default prometheus metrics

--- cmd/nanomart/main.go
+++ cmd/nanomart/main.go
@@ -4,16 +4,30 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/kilchik/nanomart/pkg/api"
 	"github.com/kilchik/nanomart/pkg/storage"
 )
 
-const addr = "0.0.0.0:5301"
+const (
+	addrApp     = "0.0.0.0:5301"
+	addrMetrics = "0.0.0.0:5302"
+)
 
 func main() {
+	// Initialize metrics server
+	router := mux.NewRouter()
+	router.Handle("/metrics", promhttp.Handler())
+	metricsSrv := http.Server{
+		Addr:    addrMetrics,
+		Handler: router,
+	}
+	go metricsSrv.ListenAndServe()
+
 	db := sqlx.MustOpen("sqlite3", "nanomart.db")
 
 	store := storage.New(db)
@@ -21,8 +35,8 @@ func main() {
 
 	http.HandleFunc("/api/v1/order", app.HandleCreateOrder)
 
-	log.Printf("listening %v", addr)
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		log.Fatalf("listen %s: %v", addr, err)
+	log.Printf("listening %v", addrApp)
+	if err := http.ListenAndServe(addrApp, nil); err != nil {
+		log.Fatalf("listen %s: %v", addrApp, err)
 	}
 }
--- go.mod
+++ go.mod
@@ -8,10 +8,24 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/mock v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/pressly/goose/v3 v3.7.0 // indirect
+	github.com/prometheus/client_golang v1.14.0
 	github.com/stretchr/testify v1.8.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
